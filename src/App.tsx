5cd011a6ac7e0404ee371368e43550ebc3c1cb47
Olanrewajuemmanuel <63521462+Olanrewajuemmanuel@users.noreply.github.com>
2024-08-05 01:54:47 +0100

    chore: Add typescript and tailwind dependencies

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import "./App.css";
+import { useEffect, useState } from "react";
+
+function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000")
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+      })
+      .catch((err) => console.error(err));
+  }, []);
+
+  return <div>{data}</div>;
+}
+
+export default App;
