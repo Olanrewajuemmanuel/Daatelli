2c45adc0159662abe0e18fadafab59884fd175e3
Olanrewajuemmanuel <63521462+Olanrewajuemmanuel@users.noreply.github.com>
2024-08-24 00:31:52 +0100

    feat: Update File upload component
    Conditionally render private copy checkbox

--- src/components/research/common/UploadFileStep.tsx
+++ src/components/research/common/UploadFileStep.tsx
@@ -7,7 +7,7 @@ import InputError from "../../InputError";
 
 
 function UploadFileStep() {
-    const { register, formState: { errors } } = useFormContext<UploadFileSchemaType>();
+    const { register, getValues, formState: { errors } } = useFormContext<UploadFileSchemaType>();
     return (
         <div>
             <div>
@@ -15,11 +15,16 @@ function UploadFileStep() {
                 <label htmlFor="files">Upload files (max. 200MB total for a max of 10 files) <InfoItem message="Upload PDFs, CSVs, Excel sheets or docx files. By uploading each file, you confirm that you have reviewed and verified each file. You also agree to allow Air-data to extract from each file any data, text snippets, tables, graphs, images, captions and metadata as applicable." /></label>
                 {errors && <InputError message={errors.files?.message?.toString()} />}
             </div>
-            <div>
-                <input {...register('privateCopy')} type="checkbox" name="privateCopy" />
-                <label htmlFor="privateCopy">Upload as a private copy <InfoItem message="Private copies are only visible to your account and can be viewed in this Finding details once created." /></label>
-                {errors && <InputError message={errors.privateCopy?.message?.toString()} />}
-            </div>
+            {
+                getValues('files')!.length > 0 && (
+                    <div>
+                        <input {...register('privateCopy')} type="checkbox" name="privateCopy" />
+                        <label htmlFor="privateCopy">Upload as a private copy <InfoItem message="Private copies are only visible to your account and can be viewed in this Finding details once created." /></label>
+                        {errors && <InputError message={errors.privateCopy?.message?.toString()} />}
+                    </div>
+                )
+            }
+
             <SelectMultipleResearchers />
             <div>
                 <label htmlFor="doiOrLink">Add a DOI or a link to your article <InfoItem message="Add links to an external site if the article belongs to an online journal or is available online. Kindly ensure links submitted are authentic, credible and not expired" /></label>
