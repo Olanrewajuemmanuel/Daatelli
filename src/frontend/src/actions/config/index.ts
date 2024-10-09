0622023b0c4d796fb088bf31e54e5261f5285646
Olanrewajuemmanuel <63521462+Olanrewajuemmanuel@users.noreply.github.com>
2024-10-09 12:38:53 +0100

    feat: Add deployment config for Github Actions, Docker and k8s
    * refactor: Re-factor folder structure for backend
    
    * refactor: Update proxy and frontend API requests to use the api/ naming

--- src/frontend/src/actions/config/index.ts
+++ src/frontend/src/actions/config/index.ts
@@ -2,6 +2,8 @@ import axios from "axios";
 
 export const axiosPublicClient = axios.create({});
 
+const baseURL = process.env.BACKEND_URL || "http://127.0.0.1:8000";
+
 export const axiosPrivateClient = (token: string, data: unknown) =>
   axios.create({
     headers: {
